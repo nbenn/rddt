16e52fe58bd70fe63df2142145b7309d53e27168
Nicolas Bennett <nicolas.bennett@stat.math.ethz.ch>
2019-01-25 21:57:10 +0100

    improve printing

--- R/rddt.R
+++ R/rddt.R
@@ -84,6 +84,7 @@ print.rddt <- function(x, n = 10L, width = NULL, ...) {
     dat <- lapply(x$heads, head, n = n)
   }
 
+  lens <- vapply(dat, nrow, integer(1L))
   dat <- data.table::rbindlist(dat)
 
   real_inds <- Map(
@@ -91,69 +92,55 @@ print.rddt <- function(x, n = 10L, width = NULL, ...) {
     cumsum(x$nrows) - x$nrows + 1L, lens
   )
 
-  inds <- meta_col(unlist(real_inds, use.names = FALSE))
-
-  dat <- pillar::colonnade(cbind(inds, dat), has_row_id = FALSE, width = width)
-  names(dat)[1L] <- ""
-  extra <- pillar::extra_cols(pillar::squeeze(dat))
-  dat <- format(dat)
-  width <- pillar::get_max_extent(dat)
-
-  footer <- paste("# with", big_mark(x$nrows - lens), "more rows")
-  if (length(extra) > 0L) {
-    footer[length(footer)] <- paste0(
-      footer[length(footer)], ", and ", length(extra),
-      if (length(extra) > 1L) " more variables: " else " more variable: ",
-      paste0(extra, collapse = ", ")
+  real_inds <- list(
+    capital_format = rep(
+      paste(rep(" ", nchar(sum(x$nrows))), collapse = ""), 2L
+    ),
+    shaft_format = pillar::style_subtle(
+      format(unlist(real_inds, use.names = FALSE))
     )
-  }
-  footer <- wrap(footer, width)
-  footer[-length(footer)] <- paste(
-    footer[-length(footer)], paste(rep("-", width), collapse = ""), sep = "\n"
   )
-  footer <- pillar::style_subtle(footer)
+
+  term_width <- getOption("width")
+  options(width = term_width - nchar(sum(x$nrows)))
+  on.exit(options(width = term_width))
+
+  dat <- pillar::colonnade(dat, has_row_id = FALSE, width = width)
+  dat <- pillar::squeeze(dat)
+  extra <- pillar::extra_cols(dat)
+  tmp <- lapply(dat, function(x) c(list(real_inds), x))
+  attributes(tmp) <- attributes(dat)
+
+  n_tables <- length(tmp)
+  dat <- format(tmp)
+  max_width <- pillar::get_max_extent(dat)
 
   header <- paste0(
     "# An rddt [", big_mark(sum(x$nrows)), " x ",
     big_mark(ncol(x$heads[[1L]])), "] with ", length(x$nrows), " partitions"
   )
-  header <- pillar::style_subtle(wrap(header, width))
+  header <- pillar::style_subtle(wrap(header, max_width))
+  extra_rows <- paste("# with", big_mark(x$nrows - lens), "more rows")
+  footer <- extra_rows[length(extra_rows)]
+  if (length(extra) > 0L) {
+    footer <- paste0(
+      footer, ", and ", length(extra),
+      if (length(extra) > 1L) " more variables: " else " more variable: ",
+      paste0(extra, collapse = ", ")
+    )
+  }
+  footer <- pillar::style_subtle(wrap(footer, max_width))
+  extra_rows <- pillar::style_subtle(wrap(extra_rows, max_width))
 
-  breaks <- cumsum(lens) + 2L
-  breaks <- breaks + seq_along(breaks) - 1L
-
-  out <- dat
-  for (i in seq_along(breaks))
-    out <- append(out, footer[i], breaks[i])
-  out <- c(header, out)
-  class(out) <- class(dat)
-  print(out)
+  tmp <- split(dat, rep(seq_len(n_tables), each = 2L + sum(lens)))
+  tmp <- lapply(tmp, split, c(1L, 1L, rep(seq_along(lens), lens)))
+  tmp <- lapply(tmp, function(y) Map(c, y, extra_rows))
+  tmp <- c(header, unlist(tmp, recursive = TRUE))
+  tmp[length(tmp)] <- footer
+  class(tmp) <- class(dat)
+  print(tmp)
 
   invisible(x)
-}
-
-meta_col <- function(ind) {
-  stopifnot(is.integer(ind))
-  structure(ind, class = "meta_col")
-}
-
-#' @importFrom pillar pillar_shaft
-#' @export
-pillar_shaft.meta_col <- function(x, ...) {
-  x <- pillar::style_subtle(format(x))
-  pillar::new_pillar_shaft_simple(x, ...)
-}
-
-#' @importFrom pillar type_sum
-#' @export
-type_sum.meta_col <- function(x) {
-  structure("", class = "ts_meta_col")
-}
-
-#' @importFrom pillar format_type_sum
-#' @export
-format_type_sum.ts_meta_col <- function(x, ...) {
-  ""
 }
 
 big_mark <- function(x, ...) {
